428d8c7bffbbc75fcaa815a09a87e577513d2472
tekuconcept <electro_madness@yahoo.com>
2018-01-19 22:17:38 +0000

    Added Version Header For Convinience

---
+++ Sockets/Include/SocVersion.h
@@ -0,0 +1,44 @@
+#ifndef __IMPACT_SOCKETS_VER_H
+#define __IMPACT_SOCKETS_VER_H
+/***************************************************************************
+ *
+ *  Project
+ *   _____                           _   _____            _        _       
+ *  |_   _|                         | | /  ___|          | |      | |      
+ *    | | _ __ ___  _ __   __ _  ___| |_\ `--.  ___   ___| | _____| |_ ___ 
+ *    | || '_ ` _ \| '_ \ / _` |/ __| __|`--. \/ _ \ / __| |/ / _ \ __/ __|
+ *   _| || | | | | | |_) | (_| | (__| |_/\__/ / (_) | (__|   <  __/ |_\__ \
+ *   \___/_| |_| |_| .__/ \__,_|\___|\__\____/ \___/ \___|_|\_\___|\__|___/
+ *                 | |                                                     
+ *                 |_|
+ *
+ * Created by TekuConcept, et al. July 21, 2017
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the LICENSE file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
+
+/* This header file contains nothing but socket version info. */
+
+/* This is the version number of the libcurl package from which this header
+   file origins: */
+#define SOCKET_VERSION "0.2.0"
+
+/* The numeric version number is also available "in parts" by using these
+   defines: */
+#define SOCKET_VERSION_MAJOR 0
+#define SOCKET_VERSION_MINOR 2
+#define SOCKET_VERSION_PATCH 0
+
+/* The numeric version number represented in hex as 0xXXYYZZ: */
+#define SOCKETS_VERSION_NUM 0x000200
+
+#endif /* __IMPACT_SOCKETS_VER_H */
