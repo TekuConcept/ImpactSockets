f75859a4fe9d76c2d327543ab6b0601f1d4dfd29
TekuConcept <electro_madness@yahoo.com>
2018-06-21 16:06:58 -0600

    Dev Checkpoint 062120181606

---
+++ Sockets/Include/UdpSocket.h
@@ -0,0 +1,17 @@
+/**
+ * Created by TekuConcept on June 21, 2018
+ */
+
+#ifndef _UDP_SOCKET_H_
+#define _UDP_SOCKET_H_
+
+namespace Impact {
+	class UdpSocket {
+
+	public:
+		UdpSocket();
+		virtual ~UdpSocket();
+	};
+}
+
+#endif
---
+++ Sockets/Source/UdpSocket.cpp
@@ -0,0 +1,11 @@
+/**
+ * Created by TekuConcept on June 21, 2018
+ */
+
+#include "UdpSocket.h"
+
+using namespace Impact;
+
+UdpSocket::UdpSocket() {}
+
+UdpSocket::~UdpSocket() {}
