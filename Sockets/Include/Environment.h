e018ec51c42cb0fa94bef4674f267c0b1fdbd2b8
tekuconcept <electro_madness@yahoo.com>
2018-07-07 18:25:11 +0000

    Dev Checkpoint 070720181225

---
+++ Sockets/Include/Environment.h
@@ -0,0 +1,42 @@
+/**
+* Created by TekuConcept on July 8, 2017
+*/
+
+#ifndef _ENVIRONMENT_H_
+#define _ENVIRONMENT_H_
+
+#if defined(_WIN32) | defined(_WIN64)
+  #define __WINDOWS__
+#elif defined(__linux__) | defined(__linux) | defined(linux)
+  #define __LINUX__
+/*
+#else __APPLE__
+*/
+#endif
+
+#ifdef __WINDOWS__
+  #pragma warning (disable : 4514)
+  #pragma warning (disable : 4710)
+  #pragma warning (disable : 4626)
+  #pragma warning (disable : 4996)
+#endif
+
+#ifndef SOC_DECLSPEC
+	#ifdef __WINDOWS__
+		#ifdef EXPORT
+			#define SOC_DECLSPEC __declspec(dllexport)
+		#elif defined(IMPORT)
+			#define SOC_DECLSPEC __declspec(dllimport)
+		#else /* STATIC */
+			#define SOC_DECLSPEC
+		#endif
+	#else
+    #define SOC_DECLSPEC
+	#endif // __WINDOWS__
+#endif // SOC_DECLSPEC
+
+#ifndef UNUSED
+#define UNUSED(x) (void)x
+#endif
+
+#endif // _ENVIRONMENT_H_
