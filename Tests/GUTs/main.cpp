9b6587bf02fce37902713303a09061a823556057
TekuConcept <electro_madness@yahoo.com>
2018-07-28 15:35:13 -0600

    Dev Checkpoint 072820181535

---
+++ Tests/GUTs/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * Created by TekuConcept on July 28, 2018
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    // GUTs - Google Unit Tests
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
