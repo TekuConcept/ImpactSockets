7c05dd84b776fd5866a787bb18436891ab77f0a5
TekuConcept <TekuConcept@ymail.com>
2019-01-21 14:26:08 -0700

    Dev Checkpoint 201901211426

--- FindImpactSockets.cmake
+++ FindImpactSockets.cmake
@@ -12,44 +12,54 @@
 #   SOCKETS_LIBRARIES      - List of libraries when using ImpactSockets.
 #   SOCKETS_FOUND          - True if ImpactSockets found.
 #   SOCKETS_VERSION_STRING - the version of ImpactSockets found.
+#
 
-# Look for the header file.
-find_path(SOCKETS_INCLUDE_DIR
-    PATHS
-    /usr/include/sockets
-    /usr/local/include/sockets
-    NAMES basic_socket
-)
-mark_as_advanced(SOCKETS_INCLUDE_DIR)
-
-# Look for the library (sorted from most current/relevant entry to least).
-find_library(SOCKETS_LIBRARY NAMES
-    ImpactSockets
-    # Windows MSVC prebuilts:
-    # <->lib
-    # lib<->_imp
-    # <->lib_static
-    # Windows older "Win32 - MSVC" prebuilts
-    # lib<->
-)
-mark_as_advanced(SOCKETS_LIBRARY)
-
-IF (SOCKETS_INCLUDE_DIR)
-    IF (EXISTS "${SOCKETS_INCLUDE_DIR}/SocVersion.h")
-    FILE(STRINGS "${SOCKETS_INCLUDE_DIR}/SocVersion.h" soc_version_str REGEX "^#define[\t ]+SOCKET_VERSION[\t ]+\".*\"")
+IF (SOCKETS_LIBRARIES AND SOCKETS_INCLUDE_DIRS)
+    SET(SOCKETS_FOUND TRUE)
+ELSE (SOCKETS_LIBRARIES AND SOCKETS_INCLUDE_DIRS)
+    FIND_PATH(SOCKETS_INCLUDE_DIR
+        NAMES basic_socket
+        PATHS
+        /usr/include/sockets
+        /usr/local/include/sockets
+    )
     
-    STRING(REGEX REPLACE "^#define[\t ]+SOCKET_VERSION[\t ]+\"([^\"]*)\".*" "\\1" SOCKET_VERSION_STRING "${soc_version_str}")
-    UNSET(soc_version_str)
+    FIND_LIBRARY(SOCKETS_LIBRARY
+        NAMES ImpactSockets
+        PATHS
+        /usr/lib
+        /usr/local/lib
+        # Windows MSVC prebuilts:
+        # <->lib
+        # lib<->_imp
+        # <->lib_static
+        # Windows older "Win32 - MSVC" prebuilts
+        # lib<->
+    )
+    
+    #IF (SOCKETS_INCLUDE_DIR)
+    #    IF (EXISTS "${SOCKETS_INCLUDE_DIR}/SocVersion.h")
+    #        FILE(STRINGS "${SOCKETS_INCLUDE_DIR}/SocVersion.h" soc_version_str REGEX "^#define[\t ]+SOCKETS_VERSION[\t ]+\".*\"")
+    #        STRING(REGEX REPLACE "^#define[\t ]+SOCKETS_VERSION[\t ]+\"([^\"]*)\".*" "\\1" SOCKETS_VERSION_STRING "${soc_version_str}")
+    #        UNSET(soc_version_str)
+    #    ENDIF ()
+    #ENDIF ()
+    
+    IF (SOCKETS_INCLUDE_DIR AND SOCKETS_LIBRARY)
+        SET(SOCKETS_FOUND TRUE)
     ENDIF ()
-ENDIF ()
 
-#include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Sockets
-                                  REQUIRED_VARS SOCKETS_LIBRARY SOCKETS_INCLUDE_DIR
-                                  VERSION_VAR SOCKETS_VERSION_STRING)
-
-IF (SOCKETS_FOUND)
-    SET(SOCKETS_LIBRARIES ${SOCKETS_LIBRARY})
     SET(SOCKETS_INCLUDE_DIRS ${SOCKETS_INCLUDE_DIR})
-ENDIF ()
+
+    IF (SOCKETS_LIBRARY)
+        SET(SOCKETS_LIBRARIES ${SOCKETS_LIBRARIES} ${SOCKETS_LIBRARY})
+    ENDIF (SOCKETS_LIBRARY)
+
+    INCLUDE(FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(ImpactSockets DEFAULT_MSG
+        SOCKETS_LIBRARIES SOCKETS_INCLUDE_DIRS
+        #VERSION_VAR SOCKETS_VERSION_STRING
+    )
+
+    MARK_AS_ADVANCED(SOCKETS_INCLUDE_DIRS SOCKETS_LIBRARIES)
+ENDIF (SOCKETS_LIBRARIES AND SOCKETS_INCLUDE_DIRS)
